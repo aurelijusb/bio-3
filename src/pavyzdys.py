d8164c08a480d4dcffcbeb34246471cd02190e6d
Aurelijus Banelis <aurelijus@banelis.lt>
2012-11-25 15:06:32 +0200

    Getting sequences and (porbably) filtering by coverage.

--- src/pavyzdys.py
+++ src/pavyzdys.py
@@ -1,78 +1,52 @@
-#Sis skrtipukas vykdo NCBI blast uzklausa ir atspausdina rastus atitikmenis bei  sekas
+# Bioinformatics 2
+#
+# @author: Aurelijus Banelis 
 from genericpath import exists
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
-
-
-#enterez_query = '"serum albumin"[Protein name] AND mammals[Organism]'
-#ncbi = Bio.Blast.NCBIWWW.qblast(program="blastp" , database="swissprot", sequence="4502027", entrez_query=enterez_query,  hitlist_size=500,  expect=100.0) #seka, pagal kuria ieskoma  galima nurodyti ir kodu
-
-# Nuskaitom XML formatu parsiustus duomenis. Patartina siuos duomenis issisaugoti i faila, ir pakartotinai kreiptis tik esant butinybei.
-#blast = Bio.Blast.NCBIXML.read(ncbi)
-#for sequence in blast.alignments:
-#    print '>%s'%sequence.title # rasto atitikmens pavadinimas fasta formatu
-#    print sequence.hsps[0].sbjct # rasto atitikmens labiausiai patikimo sutampancio fragmento seka. Kiti maziau patikimi fragmentai [kuriu indeksai didesni nei 0] nedomina. 
-
-print "Started..."
-
-
-# Reading input data
 from Bio import SeqIO
+import Bio.Blast.NCBIWWW
+import Bio.Blast.NCBIXML
+
+
+# Preparing input data
+print "Started..."
 hsa_sequence = SeqIO.read("data1.fa", "fasta");
 
-#Qriteria for reliable results
-import math
-size = len(hsa_sequence);
-percent = 80
-slen = "%d:%d[slen]" % (round(size * percent / 100), size)
-print slen
 
-#from Bio.Blast import NCBIWWW
-#print "Gathering..."
-#result_handle = NCBIWWW.qblast("blastn", "nt", "8332116")
-#
-#save_file = open("my_blast.xml", "w")
-#save_file.write(result_handle.read())
-#save_file.close()
-#result_handle.close()
-
-#Searching
+# Gathering sequences
 sequences = []
-if (not exists("sequence.fasta")):
+if (exists("cached-unaligned.fa")):
+    # Loading from cache
     sequences = SeqIO.parse("cached-unaligned.fa", "fasta");
 else:
+    # Downloading sequences from online database
     print "Getting sequences from Swissport database..."
-    import Bio.Blast.NCBIWWW
-    import Bio.Blast.NCBIXML
-    ncbi = Bio.Blast.NCBIWWW.qblast(program="blastp" , database="swissprot", sequence=hsa_sequence.seq, entrez_query=slen)
-    i = 0
-    for sequence in Bio.Blast.NCBIXML.read(ncbi).alignments:
-        i += 1
-        name = sequence.title.split(' ')[0]
-        sequences.append(SeqRecord(id=name, name=name, description=sequence.title, seq=Seq(sequence.hsps[0].sbjct)))
-        if (i > 29):
-            for j, hh in enumerate(sequence.hsps):
-                print ">%d>%s" % (j, hh.sbjct)
+    ncbi = Bio.Blast.NCBIWWW.qblast(program="blastp" , database="swissprot", sequence=hsa_sequence.seq)
+        
+    # Leaving sequences with good coverage
+    for i, alignment in enumerate(Bio.Blast.NCBIXML.read(ncbi).alignments):
+        hsp = alignment.hsps[0]        
+        #FIXME: not same as in http://www.ncbi.nlm.nih.gov/blast/Blast.cgi
+        coverage = float(hsp.query_end-hsp.query_start+1)/float(len(hsa_sequence))*100
+        if (coverage > 80):
+            name = alignment.title.split(' ')[0]
+            sequence = Seq(hsp.sbjct)
+            record = SeqRecord(id=name, name=name, description=alignment.title, seq=sequence)
+            sequences.append(record)
     
-    # Caching Fasta
-    output_handle = open("example.fasta", "w")
-    SeqIO.write(sequences, output_handle, "fasta")
-    output_handle.close()
-    
-    # Caching XML
-    ncbi.seek(0, 0)
-    save_file = open("my_blast.xml", "w")
-    save_file.write(ncbi.read())
-    save_file.close()
-    ncbi.close()
+    # Caching results
+    print "Saving sequences to cache..."
+    cache_handle = open("cached-unaligned.fa", "w")
+    SeqIO.write(sequences, cache_handle, "fasta")
+    cache_handle.close()
 
-print "fiished"
 
-#    sequences = blast.alignments
-#      
+# Printing sequences
 for i, sequence in enumerate(sequences):
     print "______%d_________" % i
-    print sequence
+    print sequence.description
+    print sequence.seq
     
     
     
