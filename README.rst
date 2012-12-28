9564a5169d37ea320e48e7422e41ff31cbb85abd
Aurelijus Banelis <aurelijus@banelis.lt>
2012-12-28 23:46:54 +0200

    Saving sequence to Fasta (extracting for L1 gene)

--- README.rst
+++ README.rst
@@ -9,9 +9,37 @@ Compiling/running
 TODO
 ----
 
+Laboratorinis Darbas Nr. 3. I dalis.
+
+
+
+Užduotis
+
+ Sukurti pobes parinkimo diagnostinei sistemai programą,
+skirtą sukurti probe tinkama tik tam tikrų virusų DNR
+detekcijai, kuomet padauginimui naudojma PGR (Polimerazinę
+grandininę reakciją)
+ Sukurtą programą pritaikyti
+projektuojant diagnostinę sistemą žmogaus papilomos viruso
+(HPV) 16,18,31,33,35,51,52 tipų (sukelia gimdos kaklelio vėžį)
+infekcijai nustatyti.
+ Diagnostinė sistema turi būti specifiška
+ir turi nebūti signalo nepavojingų HPV infekcijų atvejais
+(tipai 6, 11, 40, 42, 43, 44, 57, 81).
+ Dokumentuota programa turėtų parinkinėti DNR probe L1 geno regione.
+ Atitinkama šio geno seka iš HPV 16 tipo genomo pateikta šio
+dokumento pabaigoje.
+ Šią seką galite naudoti per blast užklausas rinkdami sekas analizei.
+ Kuriant algoritmą tikslas būtų sukurti jį tokį, kad būtų parenkamas
+kuo mažesnis probiu kiekis tinkantis visų žinomų didelės rizikos
+HPV tipų variantų diagnostikai.
+
+I dalies uzduotis:
+Modifikuokite jau sukurta programa taip, kad:
 * Parsiustu visu tipu pavojingu ir nepavojingu zmogaus papilomos viruso prieinamas sekas fasta formatu į atskirus failus (vieno tipo sekos vienas failas). Paieskai naudokite dokumento pabaigoje pateikta L1 geno fragmenta.
 * Naudojant cd-hit programa ( http://weizhong-lab.ucsd.edu/cd-hit/) panaikintu identiskas sekas.
 * Sulietus atskirus failus į viena fasta faila (virsuje pavojingi tipai, zemiau nepavojingi) ir gautumet bendra sulyginima su mafft (http://mafft.cbrc.jp/alignment/software/)
+
 
     
 Questions
@@ -31,3 +59,4 @@ References
 
  * http://biopython.org/DIST/docs/tutorial/Tutorial.html
  * http://bips.u-strasbg.fr/fr/Tutorials/Comparison/Blast/blastall.html
+ * http://ghr.nlm.nih.gov/glossary=probe
--- src/U3-Aurelijus-Banelis.py
+++ src/U3-Aurelijus-Banelis.py
@@ -1,4 +1,4 @@
-# Bioinformatics 3
+# Bioinformatics, task 3
 #
 # @author: Aurelijus Banelis 
 from genericpath import exists
@@ -8,181 +8,121 @@ from Bio import SeqIO
 import Bio.Blast.NCBIWWW
 import Bio.Blast.NCBIXML
 
-# Settings
-fragment_size = 15
-
-###########################################
-# Databases:
-#    nucleotide
-#    chromosome
-#    gene
-#    probe
-#
-# http://www.ncbi.nlm.nih.gov/sites/gquery
-#
-#
-#
-###########################################
-
-
-
-
-# Preparing input data
-print "Started..."
-hsa_sequence = SeqIO.read("data3.fa", "fasta");
-
 from Bio import Entrez
-Entrez.email = 'aurelijus@banelis.lt'
-handle = Entrez.egquery(term="Human papillomavirus type")
-record = Entrez.read(handle)
+import logging
+import itertools
+import re
+import textwrap
 
-print "-------------"
 
-#for row in record["eGQueryResult"]:
-#    for key in row.keys():
-#        print "%s=%s" % (key, row[key])
-
-for row in record["eGQueryResult"]:
-    if (row['Status'] == 'Ok' and row['Count'] > 0):
-        print row['Count'], row['MenuName'], row['DbName'] 
+class HpvAnalyser:
+    # Configuration
+    gene = 'L1'
+    harmfull = [16, 18, 31, 33, 35, 51, 52]
+    safe = [6, 11, 40, 42, 43, 44, 57, 81]
+    email = 'aurelijus@banelis.lt'
+    batch_size = 3
     
-
-
-exit(0)
-
-
-
-
-
-from Bio import Entrez
-Entrez.email = 'aurelijus@banelis.lt'
-handle = Entrez.esearch(db="nucleotide",term="Human papillomavirus")
-record = Entrez.read(handle)
-
-for key in record.keys():
-    print "%s=%s" % (key, record[key])
+    # Properties
+    query = '';
+    ids = []
+    webenv = None
+    queryKey = None
     
+    def generateEntrezQuery(self):
+        """ Saves and returns query to get information about all HPV types. """
+        ending = ') AND ' + self.gene + '[Gene Name] ' + \
+                 'AND 1000:8000[Sequence Length]'
+        types = []
+        for typeNr in itertools.chain(self.harmfull, self.safe):
+            types.append('"Human papillomavirus type %d"[Organism]' % typeNr)
+        self.query = '(' + ' OR '.join(types) + ending
+        return self.query
     
-#    
-#    
-    
-##
-## Gathering sequences
-#sequences = []
-#if (exists("cached-unaligned.fa")):
-#    # Loading from cache
-#    sequences = list(SeqIO.parse("cached-unaligned.fa", "fasta"));
-#else:
-#    # Downloading sequences from online database
-#    print "Getting sequences from Swissport database..."
-#    ncbi = Bio.Blast.NCBIWWW.qblast(program="blastp" , database="gene", sequence=hsa_sequence.seq)
-#        
-#    # Leaving sequences with good coverage
-#    for i, alignment in enumerate(Bio.Blast.NCBIXML.read(ncbi).alignments):
-#        hsp = alignment.hsps[0]
-#        coverage = float(hsp.query_end-hsp.query_start+1)/float(len(hsa_sequence))*100
-#        if (coverage > 80):
-#            name = alignment.title.split(' ')[0]
-#            sequence = Seq(hsp.sbjct)
-#            record = SeqRecord(id=name, name=name, description=alignment.title, seq=sequence)
-#            sequences.append(record)
-#    
-#    # Caching results
-#    print "Saving unaligned sequences to cache..."
-#    cache_handle = open("cached-unaligned.fa", "w")
-#    SeqIO.write(sequences, cache_handle, "fasta")
-#    cache_handle.close()
-#
+    def retrieveIds(self, query=None):
+        """ Saves and returns Ids for HPV types. """
+        if (query == None):
+            query = self.query
+        Entrez.email = self.email
+        handle = Entrez.esearch(db="nucleotide", term=query, usehistory="y")
+        record = Entrez.read(handle)
+        for key, value in record.items():
+            print key, value
+        self.ids = record["IdList"]
+        self.webenv = record["WebEnv"]
+        self.queryKey = record["QueryKey"] 
+        handle.close()
+        
+    def retreveData(self):
+        """ Get Full sequences from database and convert them to Fasta """
+        self._emptyCachedFiles()
+        for start in range(0,len(self.ids),batch_size):
+            end = min(count, start+batch_size)
+            logger.info("Going to download record %i to %i" % (start+1, end))
+            fetch_handle = Entrez.efetch(db="nucleotide", rettype="gbwithparts",
+                                         retmode="text", retstart=start,
+                                         retmax=self.batch_size,
+                                         webenv=self.webenv,
+                                         query_key=self.queryKey)
+            combinedGenBank = fetch_handle.read()
+            fetch_handle.close()
+            for i, genBankText in enumerate(combinedGenBank.split("\n//\n")):
+                logger.info("Converting to Fasta %i/%i" % (start+i, end))
+                self._saveToFasta(genBankText)
+        
+    def _emptyCachedFiles(self):
+        """ Creates empty Fasta files for each HPV type. """ 
+        for typeNr in itertools.chain(self.harmfull, self.safe):
+            open(self._fastaName(typeNr), 'w').close()
+        
+    def _fastaName(self, type):
+        """ Generates fasta file name by type number """
+        return 'HPV' + str(type) + '.fa'
 
-#
-## Printing unaligned sequences
-#print "Unaligned sequences:"
-#for i, sequence in enumerate(sequences):
-#    print "[%d] Unaligned %s\n\t\t\t\t\t\t\t%s" % ((i+1), sequence.id, sequence.seq)
-#    
-#    
-## Aligning
-#if (not exists("cached-aligned.fa")):
-#    print "Aligning..."
-#    from Bio.Align.Applications import MafftCommandline
-#    import os
-#    os.environ['MAFFT_BINARIES'] = "/usr/lib/mafft/lib/mafft"
-#    mafft_exe = "/usr/bin/mafft --localpair --maxiterate 1000"
-#    in_file = "cached-unaligned.fa"
-#    mafft_cline = MafftCommandline(mafft_exe, input=in_file)
-#    stdout, stderr = mafft_cline()
-#    handle = open("cached-aligned.fa", "w")
-#    handle.write(stdout)
-#    handle.close()
-#
-## Printing aligned sequences
-#print "Aligned."
-#from Bio import AlignIO
-#multiple_seq_alignment = AlignIO.read("cached-aligned.fa", "fasta")
-#for i, record in enumerate(multiple_seq_alignment):
-#    print "[%d] Aligned %s\n\t\t\t\t\t\t\t%s" % ((i+1), record.id, record.seq)
-#
-#       
-## Calculating conservation:
-#conservations = []
-#n_y = len(sequences)
-#n_x = len(hsa_sequence)
-#for x in range(0, n_x):
-#    symbol = sequences[0][x]
-#    same = 0
-#    if (symbol != '-'):
-#        for y in range(1, n_y):
-#            if (len(sequences[y].seq) > x and sequences[y][x] == symbol):
-#                same += 1
-#        conservations.append(same)
-#    else:
-#        conservations.append(0)
-#    
-## Printing conservations:
-#print "\nSimilarity (index, symbol, column similarity, block similarity):"
-#for x in range(0, n_x):
-#    print "{0:3}".format(x),
-#print ''
-#for x in range(0, n_x):
-#    print "  %c" % hsa_sequence[x],
-#print ''
-#for x in range(0, n_x):
-#    print "{0:3}".format(conservations[x]),
-#print ''
-#    
-## Calculating conservatio sums
-#conservation_sums = []
-#current_sum = 0
-#for x in range(0, fragment_size):
-#    current_sum += conservations[x]
-#conservation_sums.append(current_sum)
-#index_min = 0
-#min_value = conservation_sums[0]
-#index_max = 0
-#max_value = conservation_sums[0]
-#
-#for x in range(fragment_size, n_x):
-#    current_sum += conservations[x]
-#    current_sum -= conservations[x - fragment_size]
-#    if (current_sum > max_value):
-#        max_value = current_sum
-#        index_max = x - fragment_size + 1
-#    elif (current_sum < min_value):
-#        min_value = current_sum
-#        index_min = x - fragment_size + 1
-#    conservation_sums.append(current_sum)
-#
-#for x in range(0, n_x - fragment_size):
-#    print "{0:3}".format(conservation_sums[x]),
-#    
-#print ''
-#
-#print "\nFormat: [index]=value | Least expected: [%d]=%d | Most expected: [%d]=%d" % (index_min, min_value, index_max, max_value)
-#import sys
-#print '\nLeast expected sequence: ',
-#for x in range(index_min, index_min + fragment_size):
-#    sys.stdout.write(hsa_sequence[x])
-#print '\nMost expected sequence: ',
-#for x in range(index_max, index_max + fragment_size):
-#    sys.stdout.write(hsa_sequence[x])
-#print ''
+    def _saveToFasta(self, genBankText):
+        """Converts GenBank format to Fasta. 
+        
+        Takes only gene part and save to file as per HPV type.
+        """
+       
+        # Parsing GenBank to Fasta sequence
+        parts = genBankText.split('/gene="' + self.gene)
+        if (len(parts) > 1):
+            boundaries = re.search('(\d+)[\.|>]+(\d+)', parts[1])
+            start = int(boundaries.group(1))
+            end = int(boundaries.group(2))
+            origin = parts[2].split("\nORIGIN      \n")[1]
+            fullFasta = ''
+            for row in origin.split('\n'):
+                fullFasta += row[11:].replace(' ', '').upper()
+            usefullFasta = fullFasta[start:end]
+            usefullFasta = "\n".join(textwrap.wrap(usefullFasta, 70))
+            
+            # Parsing GenBank to Fasta header
+            title = re.search('DEFINITION  (.+)\n', parts[0]).group(1)
+            id = re.search('  GI:(.+)\n', parts[0]).group(1)
+            type = re.search('Human papillomavirus type (\d+)', parts[0])
+            type = int(type.group(1))
+            header = '>gi|' + id + ':' + str(start) + '-' + str(end) + ' ' + title
+                     
+            # Saving to file
+            data = header + "\n" + usefullFasta + "\n"
+            file = open(self._fastaName(type), 'a')
+            file.write(data)
+            file.close()
+            return data
+
+
+# Running
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logging.info("Starting")
+hpvAnalyser = HpvAnalyser()
+
+logging.info("Generating query")
+hpvAnalyser.generateEntrezQuery()
+
+logging.info("Retrieving Ids")
+hpvAnalyser.retrieveIds()
+
+logging.info("Retrieving Corrfinates for gene")
+hpvAnalyser.retrieveData()
